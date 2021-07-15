230dbd9c15e05a9d812ead60b347dc6c0665865e
Ashwani Kumar Tiwari <43306519+Ashwani-kumar-Tiwari@users.noreply.github.com>
2021-07-15 06:24:08 +0530

    Delete secrets.js

--- src/secrets.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyCNiVN6LgtbOAfxJhOsu92guTSk6Ljq-Cc",
-    authDomain: "fir-auth-e5e69.firebaseapp.com",
-    projectId: "fir-auth-e5e69",
-    storageBucket: "fir-auth-e5e69.appspot.com",
-    messagingSenderId: "538767566472",
-    appId: "1:538767566472:web:bf6cf8af2d3bbef37ba13d"
-}
